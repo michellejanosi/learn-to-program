13ab6dedec16baaf31562ba7fe65a5878bf760c7
michellejanosi <michelle.janosi@gmail.com>
2016-06-03 15:20:07 -0400

    Solutions to chap 1 - numbers

---
+++ chap1_numbers.rb
@@ -0,0 +1,13 @@
+# Write a program that tells you:
+
+# How many hours in a year?
+puts 24 * 365
+
+# How many minutes in a decade?
+puts 60 * 24 * 365.25 * 10
+
+# How many seconds old are you?
+puts 60 * 60 * 24 * 365 * age
+
+# If I am 1246 million seconds old, how old am I?
+puts 1246000000 / (60 * 24 * 365.25)
