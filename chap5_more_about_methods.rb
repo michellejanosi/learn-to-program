a8b36e71327c5eee7ea4136ee4bb2a8752e3abf1
michellejanosi <michelle.janosi@gmail.com>
2016-06-06 14:42:25 -0400

    Added solutions to chap5 problems

--- chap5_more_about_methods.rb
+++ chap5_more_about_methods.rb
@@ -8,3 +8,16 @@ fname = gets.chomp
 puts "What is your last name?"
 lname = gets.chomp
 puts "Your first and last name is #{fname.length.to_i + lname.length.to_i} characters long."
+
+# rudely asks what you want (Angry Boss program).
+
+puts "WHAT DO YOU WANT?"
+want = gets.chomp
+puts "WHADDAYA MEAN \"#{want.upcase}\"?!?  YOU'RE FIRED!!"
+
+# displays a Table of Contents.
+
+puts "Table of Contents".center(50)
+puts "Chapter 1:  Numbers".ljust(30) + "page 1".rjust(20)
+puts "Chapter 2:  Letters".ljust(30) + "page 72".rjust(20)
+puts "Chapter 3:  Variables".ljust(30) + "page 118".rjust(20)
